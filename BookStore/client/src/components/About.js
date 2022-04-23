c903a5ad3c35d5d95444a8839b5592dd71370ea8
ABHISHEK SAINI <98455352+Abhishek0637@users.noreply.github.com>
2022-04-24 00:31:32 +0530

    Add files via upload

---
+++ BookStore/client/src/components/About.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h2 className="h2">To Know About Me</h2>
+      <a className="profile" href=" https://abhishek0637.github.io/myProfile/">Click Here</a>
+    </div>
+  );
+};
+
+export default About;
